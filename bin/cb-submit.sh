3c90d0997599d0e2442470af6a31f83ff9bbc1c6
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-08-26 08:02:03 +0200

    0.4.12 new version with CRun embedded

--- bin/cb-submit.sh
+++ bin/cb-submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-VER="0.1"
+VER="0.2"
 # TODO add them to CB config :)
 _GCP_REGION=$GCP_REGION
 _MY_REPO_NAME=$MY_REPO_NAME
@@ -10,8 +10,8 @@ FINAL_AR_IMAGE_WITHOUT_TAG="${_GCP_REGION}-docker.pkg.dev/${PROJECT_ID}/${_MY_RE
 # TODO remove this after accepting in your heart that CB does NOT accept parametric `images:` stanza.
 FINAL_AR_IMAGE_LATEST="$FINAL_AR_IMAGE_WITHOUT_TAG:latest"
 
-echo '1. variable _FINAL_AR_IMAGE_WITHOUT_TAG:' "${FINAL_AR_IMAGE_WITHOUT_TAG}"
-echo '2. fixed    _FINAL_AR_IMAGE_WITHOUT_TAG:' "europe-west1-docker.pkg.dev/ror-goldie/ror7-gemini-saga-gallery/saga-gallery"
+#echo '1. variable _FINAL_AR_IMAGE_WITHOUT_TAG:' "${FINAL_AR_IMAGE_WITHOUT_TAG}"
+#echo '2. fixed    _FINAL_AR_IMAGE_WITHOUT_TAG:' "europe-west1-docker.pkg.dev/ror-goldie/ror7-gemini-saga-gallery/saga-gallery"
 
 # To be submitted by Riccardo locally with hydrated vars which are NOT available solely on gihtub
 echodo gcloud --project "$PROJECT_ID" builds submit --config=cloudbuild.yaml \
