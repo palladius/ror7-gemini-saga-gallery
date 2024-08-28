aad89659358a1863b92543dbf89c493831304e06
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-08-28 07:12:16 +0200

    0.4.23 succesfully terraformed GCS without IAM

--- iac/terraform/01-gcs.tf
+++ iac/terraform/01-gcs.tf
@@ -6,6 +6,7 @@
 resource "google_storage_bucket" "dev_bucket" {
   name         = format("%sricc-public-%s-development", var.terraform_prefix, var.app_name)
   location     = "EU"  # Public buckets can only be in multi-region locations
+  project = var.project_id
   storage_class = "standard"
   uniform_bucket_level_access = true
   force_destroy = true
@@ -20,6 +21,7 @@ resource "google_storage_bucket" "dev_bucket" {
 resource "google_storage_bucket" "prod_bucket" {
   name         = format("%sricc-public-%s-production",  var.terraform_prefix, var.app_name)
   location     = "EU"  # Public buckets can only be in multi-region locations
+  project = var.project_id
   storage_class = "standard"
   uniform_bucket_level_access = true
   force_destroy = true
@@ -30,14 +32,15 @@ resource "google_storage_bucket" "prod_bucket" {
   }
 }
 
-resource "google_storage_default_object_access_control" "public_rule_dev" {
-  bucket = google_storage_bucket.dev_bucket.name
-  role   = "READER"
-  entity = "allUsers"
- }
-resource "google_storage_default_object_access_control" "public_rule_prod" {
-  bucket = google_storage_bucket.prod_bucket.name
-  role   = "READER"
-  entity = "allUsers"
- }
+# │ Error: Error creating DefaultObjectAccessControl: googleapi: Error 400: Cannot use ACL API to update bucket policy when uniform bucket-level access is enabled. Read more at https://cloud.google.com/storage/docs/uniform-bucket-level-access, invalid
+# resource "google_storage_default_object_access_control" "public_rule_dev" {
+#   bucket = google_storage_bucket.dev_bucket.name
+#   role   = "READER"
+#   entity = "allUsers"
+#  }
+# resource "google_storage_default_object_access_control" "public_rule_prod" {
+#   bucket = google_storage_bucket.prod_bucket.name
+#   role   = "READER"
+#   entity = "allUsers"
+#  }
 
